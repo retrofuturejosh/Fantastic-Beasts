ef435188ed7a207e88722f7420f4baa30cfa4850
MiSTERSPOONS <jonathanahnmusic@gmail.com>
2017-11-01 16:50:20 -0400

    user & beasts express route complete

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
 module.exports = router
+const { User } = require('../db/models')
 
 router.get('/', (req, res, next) => {
   User.findAll({
@@ -11,4 +11,33 @@ router.get('/', (req, res, next) => {
   })
     .then(users => res.json(users))
     .catch(next)
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then(user => res.json(user))
+    .catch(next)
 })
+
+router.post('/', (req, res, next) => {
+  User.findOrCreate({ where: req.body })
+    .spread(( user, _) => res.json(user))
+    .catch(next);
+});
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then(user => user.update(req.body))
+    .then(updatedUser => res.json(updatedUser))
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then(user => user.destroy())
+    .then(() => res.json({ message: 'Delete Successful' }))
+    .catch(next)
+});
