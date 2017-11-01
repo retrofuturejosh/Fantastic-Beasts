e9691e9f77449f814ba24171a6f1d1360d00d747
Joshua Sohn <joshuadsohn@gmail.com>
2017-11-01 11:42:49 -0400

    creates associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,7 @@
-const User = require('./user')
+const User = require('./user');
+const Beast = require('./beast');
+const Review = require('./review');
+const Order = require('./order');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +16,39 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+ //order table has user Id
+Order.belongsTo(User);
+
+//association table to look up all users orders
+User.hasMany(Order);
+
+//'cart' table has an order id
+Order_Beasts.belongsTo(Order)
+
+//'cart' has beast id
+Order_Beasts.belongsTo(Beast);
+
+//order table will have an order_beasts id
+Order.belongsTo(Order_Beasts);
+
+//association table of all the user's reviews
+User.hasMany(Review);
+
+//association table of all the beast's reviews
+Beast.hasMany(Review);
+
+//review table has an author id from user id
+Review.belongsTo(User, {as: 'author'});
+
+//review table has a reviewee id from beast id
+Review.belongsTo(Beast, {as: 'reviewee'});
+
+
 module.exports = {
-  User
+  User,
+  Beast,
+  Review,
+  Order,
+  Order_Beasts
 }
